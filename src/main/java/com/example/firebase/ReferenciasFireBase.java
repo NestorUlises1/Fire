5f5d41a144559c09bb86d07d2d2959c59c138206
NestorUlises1 <90734395+NestorUlises1@users.noreply.github.com>
2022-08-15 10:38:43 -0600

    Delete src directory

--- src/main/java/com/example/firebase/ReferenciasFireBase.java
+++
@@ -1,7 +0,0 @@
-package com.example.firebase;
-
-public class ReferenciasFireBase {
-    final static public String URL_DATABASE = "https://agenda-9a926-default-rtdb.firebaseio.com/";
-    final static public String DATABASE_NAME = "Agenda";
-    final static public String TABLE_NAME = "Contactos";
-}
